14163c40e4e870efc9da800f878db76efacd120d
TothZsofia <tzsofia07@gmail.com>
2024-04-04 12:03:28 +0200

    hehe

--- szamologep/MainWindow.xaml.cs
+++ szamologep/MainWindow.xaml.cs
@@ -16,6 +16,10 @@ namespace szamologep
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        static bool feladat = false;
+        static string ertek;
+        static int vegertek = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -23,7 +27,125 @@ namespace szamologep
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
+            if (feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "1";
+            ertek +="1";
+
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            if(feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "2";
+            ertek +="2";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            if(feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "3";
+            ertek +="3";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            if(feladat== true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "4";
+            ertek +="4";  
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            if (feladat== true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "5";
+            ertek +="5";
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            if (feladat==true) 
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "6";
+            ertek +="6";
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            if (feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat = false;
+            }
+            lblkiiras.Content+="7";
+            ertek +="7";
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            if (feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "8";
+            ertek +="8";
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            if (feladat == true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "9";
+            ertek +="9";
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            if(feladat==true)
+            {
+                lblkiiras.Content = "";
+                feladat=false;
+            }
+            lblkiiras.Content += "0";
+            ertek +="0";
+        }
+
+        private void btnosszeadas_Click(object sender, RoutedEventArgs e)
+        {
+           
+            lblkiiras.Content += "+";
+        }
+
+        private void btnkivonas_Click(object sender, RoutedEventArgs e)
+        {
             
+            lblkiiras.Content+= "-";
         }
     }
 }
